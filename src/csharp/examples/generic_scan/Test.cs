2ede1183f5c95a1078ca2a959c4b4a7065d87c3b
Aaron Brewbaker <brewbaker.aaron@gmail.com>
2015-04-06 12:36:40 -0400

    init Scan.cs sum test

--- src/csharp/examples/generic_scan/Test.cs
+++ src/csharp/examples/generic_scan/Test.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Tutorial.Cs.examples.generic_scan
 {
     class Test
     {
+        public double[] Gen(int n)
+        {
+            var vals = new double[n];
+            var rng = new Random();
+            for (var i = 0; i < n; i++)
+                vals[i] = rng.NextDouble();
+            return vals;
+        }
+
+        // TODO: Add functionality for operations other than sum
+        public double[] cpuScan(double[] input, bool inclusive)
+        {
+            int idx = 1;
+            var result = input.Select(_ => input.Take(idx++).Sum()).ToArray();
+            return inclusive ? result.Skip(1).ToArray() : result;
+        }
+
+        [Test]
+        public void InclusiveSum()
+        {
+            var nums = new[] { 1, 2, 8, 128, 100, 1024 };
+            foreach (int n in nums)
+            {
+                var values = Gen(n);
+                var dr = ScanApi.Sum(values, true);
+                var hr = cpuScan(values, true);
+                
+                //Console.WriteLine("hr, dr ===> {0}, {1}", hr, dr);
+                
+                for(var i = 0; i < hr.Length; i++)
+                    Assert.AreEqual(hr[i], dr[i], 1e-12);
+            }
+        }
     }
 }
